bb54c039bb13e80c4f0d0ff6d0a949e61ad6fb3a
dvgiap <juuzou152@gmail.com>
2024-10-05 21:50:16 +0700

    hw kiem thu chuc nang

---
+++ chicken_rice.py
@@ -0,0 +1,21 @@
+def calculate_total_price(x, y):
+    meal_price = 50000
+
+    if x < 0 or x > 10 or y < 0 or y > 10:
+        print("Input khong hop le")
+        return
+    elif y == 0:
+        total_price = meal_price * x
+    elif 0 < y <= 2:
+        total_price = meal_price * x + 10000
+    elif 2 < y <= 10:
+        total_price = meal_price * x + 10000 + (y - 2) * 5000
+    
+    return total_price
+
+x = int(input("Nhap so suat com ga (1-10): "))
+y = float(input("Nhap khoang cach (km) (0-10): "))
+
+total_price = calculate_total_price(x, y)
+
+print(f"Tong so tien can phai tra: {total_price} VND")
